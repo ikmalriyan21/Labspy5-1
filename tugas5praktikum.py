4c2246d6824153c594b29bfcd4ae7e27208e9fa5
mastio1836 <prasetyotio775@gmail.com>
2019-11-24 23:15:49 -0800

    Tugaspraktikum5

---
+++ tugas5praktikum.py
@@ -0,0 +1,31 @@
+nilai =[]
+ulang = True
+
+while ulang:
+    nama = input("Masukan Nama:")
+    nim  = input("Masukan Nim:")
+    tugas = int(input("Masukan Nilai Tugas"))
+
+    uts = int(input("Masukan Nilai UTS:"))
+    uas = int(input("Masukan Nilai UTS:"))
+    akhir = (tugas * 30/100) + (uts * 35/100) + (uas * 35/100)
+
+    nilai.append([nama, nim, int(tugas), int(uts), int(uas), int(akhir)])
+    if(input("Tambah data (y/t)?") == 't'):
+        ulang = False
+
+        print("\nDaftar Nilai Mahasiswa")
+        print("==========================================================================")
+
+        print("|No. |  Nama |  Nim |  Tugas |  Uas |  Akhir |")
+        print("==========================================================================")
+        i = 0
+        for item in nilai:
+            i += 1
+        print("| {no:2d} | {nama:12s} | {nim:9s} | {tugas:7d} | {uts:5d} | {akhir:6.2f} |"
+        format(no=i,nama=item[0], nim=item[1],tugas=item[2],uts=item[3],uas=item[4],akhir[5]))
+        print("==========================================================================")
+
+
+
+
